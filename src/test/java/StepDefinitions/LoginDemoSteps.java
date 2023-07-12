a425eb407c6dfa852bb13fa2167332c6c212e49d
Yash Harjani <yashharjani2001@gmail.com>
2023-07-12 14:01:33 +0530

    Updating

--- src/test/java/StepDefinitions/LoginDemoSteps.java
+++ src/test/java/StepDefinitions/LoginDemoSteps.java
@@ -10,41 +10,41 @@ import io.cucumber.java.en.*;
 
 public class LoginDemoSteps {
 	
-//	WebDriver driver = null;
-//	
-//	@Given("browser is open")
-//	public void browser_is_open() {
-//		String projectPath = System.getProperty("user.dir");
-//		System.setProperty("webdriver.chrome.driver", projectPath+"/src/test/resources/Drivers/chromedriver.exe");
-//		driver = new ChromeDriver();
-//		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-//		driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
-////		driver.manage().window().fullscreen();
-//	}
-//	
-//	@And("user is on login page")
-//	public void user_is_on_login_page() {
-//		driver.get("https://example.testproject.io/web/");
-//	}
-//
-//	@When("^user enters (.*) and (.*)$")
-//	public void user_enters_automation_testing_and(String username, String password) throws InterruptedException {
-//		driver.findElement(By.id("name")).sendKeys(username);
-//		driver.findElement(By.id("password")).sendKeys(password);
-//		Thread.sleep(1000);
-//	}
-//
-//	@And("user clicks on login")
-//	public void user_clicks_on_login() throws InterruptedException {
-//		driver.findElement(By.id("login")).click();
-//		Thread.sleep(1000);
-//	}
-//
-//	@Then("user should be navigated to home page")
-//	public void user_should_be_navigated_to_home_page() {
-//		driver.findElement(By.id("logout")).isDisplayed();
-//		driver.close();
-//		driver.quit();
-//	}
+	WebDriver driver = null;
+	
+	@Given("browser is open")
+	public void browser_is_open() {
+		String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectPath+"/src/test/resources/Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
+//		driver.manage().window().fullscreen();
+	}
+	
+	@And("user is on login page")
+	public void user_is_on_login_page() {
+		driver.get("https://example.testproject.io/web/");
+	}
+
+	@When("^user enters (.*) and (.*)$")
+	public void user_enters_automation_testing_and(String username, String password) throws InterruptedException {
+		driver.findElement(By.id("name")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		Thread.sleep(1000);
+	}
+
+	@And("user clicks on login")
+	public void user_clicks_on_login() throws InterruptedException {
+		driver.findElement(By.id("login")).click();
+		Thread.sleep(1000);
+	}
+
+	@Then("user should be navigated to home page")
+	public void user_should_be_navigated_to_home_page() {
+		driver.findElement(By.id("logout")).isDisplayed();
+		driver.close();
+		driver.quit();
+	}
 
 }
--- src/test/java/StepDefinitions/TestRunnerForHooks.java
+++ src/test/java/StepDefinitions/TestRunnerForHooks.java
@@ -3,6 +3,7 @@ package StepDefinitions;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/HooksDemo/HooksDemoLogin.feature",
